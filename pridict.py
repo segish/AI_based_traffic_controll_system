9c7ea4c8646f018a9eeac38117f76b2e0feed425
Tsega Tigneh <tsegatigneh08@gmail.com>
2024-07-25 09:19:38 +0300

    first commit

--- pridict.py
+++ pridict.py
@@ -6,17 +6,12 @@ import cv2
 
 VIDEOS_DIR = os.path.join('.', 'videos')
 
-video_path = os.path.join(VIDEOS_DIR, 'los_angeles.mp4')
-video_path_out = '{}_out.mp4'.format(video_path)
-
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture('./videos/lane1.mp4')
 ret, frame = cap.read()
-H, W, _ = frame.shape
-out = cv2.VideoWriter(video_path_out, cv2.VideoWriter_fourcc(*'MP4V'), int(cap.get(cv2.CAP_PROP_FPS)), (W, H))
 
-model_path = os.path.join('bestbest.pt')
+model_path = os.path.join('yolov8x.pt')
 
-model = YOLO("bestbest.pt")
+model = YOLO("yolov8x.pt")
 
 threshold = 0.3
 
@@ -45,5 +40,5 @@ while ret:
     ret, frame = cap.read()
 
 cap.release()
-out.release()
+# out.release()
 cv2.destroyAllWindows()
